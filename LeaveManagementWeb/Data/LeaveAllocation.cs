7ff46208d82bb4eedffb16d7646035ec0d729163
EdMagee10 <104154499+EdMagee10@users.noreply.github.com>
2022-12-31 16:45:49 +0000

    Added Database Tables and Migrations, End Implement Database Section

---
+++ LeaveManagementWeb/Data/LeaveAllocation.cs
@@ -0,0 +1,13 @@
+﻿namespace LeaveManagementWeb.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int Id { get; set; }        
+        public int NumberOfDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public string EmployeeId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
