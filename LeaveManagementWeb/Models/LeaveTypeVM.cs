8efb72569030f8eb7410c02ff4b5f5f3208e7b3e
EdMagee10 <104154499+EdMagee10@users.noreply.github.com>
2023-01-03 18:18:02 +0000

    Modified the crud screens and added datatables and swal. End of Leave Type Management Module

---
+++ LeaveManagementWeb/Models/LeaveTypeVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementWeb.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please Enter A Valid Number")]
+        public int DefaultDays { get; set; }
+    }
+}
